b85c96cee85190d1280cac3b6f3f360bc1d33626
erfa <qerfari@gmail.com>
2018-04-02 03:43:21 +0300

    Modified templates to display posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
 ]
